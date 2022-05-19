8d657dfa2f6c1cc1fd97c13f5fb3cf9d07e90836
c-mccormick007 <c.mccormick1295@gmail.com>
2022-05-19 02:52:08 -0500

    add book button with dynamic cards displaying books

--- index.js
+++ index.js
@@ -1,41 +1,82 @@
 console.log("connected");
 
 let myLibrary = [];
+let id = 1;
 
-function Book(name, author, pages, isRead){
+function Book(name, author, pages){
     this.bookName = name;
     this.author = author;
     this.pages = pages;
-    this.isRead = isRead;
+    this.isRead = false;
+    this.id = id;
 }
 
-let addBookToLibrary = (name, author, pages, isRead) => {
-    const tempBook = new Book(name, author, pages, isRead)
+let addBookToLibrary = (name, author, pages) => {
+    const tempBook = new Book(name, author, pages)
     myLibrary.push(tempBook);
+    id++;
 }
 
-addBookToLibrary(34,23,23,true);
-addBookToLibrary(34,23,23,true);
-addBookToLibrary(34,23,23,true);
-addBookToLibrary(34,23,23,true);
-addBookToLibrary(34,23,23,true);
-addBookToLibrary(34,23,23,true);
-console.log(myLibrary)
+const bookButton = document.querySelector('.newBook');
+const deleteButton = document.querySelector('.deleteButton');
+let modal = document.querySelector('#modal');
+let card = document.querySelector('card');
+
+
+bookButton.addEventListener('click', () => {
+    modal.classList.add('modal-visible');
+    modal.focus();
+})
+
+
+const form = document.getElementById('bookForm');
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
+form.addEventListener('submit', () => {
+    addBookToLibrary(form[0].value, form[1].value, form[2].value);
+    displayBooks();
+    modal.classList.remove('modal-visible');
+    
+})
+
+let bookNum = 0;
 
 let displayBooks = () => {
-    for (i in myLibrary){
-        let div = document.createElement("div");
-        document.getElementById("bookCards").appendChild(div);
-        div.classList.add("card");
-    }
+    
+    let div = document.createElement("div");
+    div.classList.add("card");
+    div.classList.add(id-1);
+    document.getElementById("bookCards").appendChild(div);
+    let childName = document.createElement("div");
+    childName.classList.add("bookName");
+    childName.textContent = myLibrary[bookNum].bookName;
+    div.appendChild(childName);
+    let childAuthor = document.createElement("div");
+    childAuthor.classList.add("author");
+    childAuthor.textContent = myLibrary[bookNum].author;
+    div.appendChild(childAuthor);
+    let childPages = document.createElement("div");
+    childPages.textContent = myLibrary[bookNum].pages;
+    childPages.classList.add("pages");
+    div.appendChild(childPages);
+    let childRead = document.createElement("div");
+    childRead.textContent = myLibrary[bookNum].isRead;
+    childRead.classList.add("read");
+    div.appendChild(childRead);
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "Remove";
+    deleteButton.setAttribute("data-id", bookNum);
+    deleteButton.addEventListener('click', (e) => {
+        const currentTarget = e.target.parentNode
+        console.log(currentTarget);
+        console.log(myLibrary);
+        //myLibrary.splice(, 1)
+        currentTarget.remove();
+    })
+    div.appendChild(deleteButton);
+    bookNum++;
 }
 
-displayBooks();
-
-/* 
-<div class="card">
-    <div class="bookTitle">Asdfghjkl</div>
-    <div class="author">Stephen Hawking</div>
-    <div class="pages">345</div>
-    <div class="read">true</div>
-</div> */
+function removeBook(array, position){
+    array.splice(position);
+}
